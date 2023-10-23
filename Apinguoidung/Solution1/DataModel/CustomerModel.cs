e6343dcbc39ab285892662a4fb928843fa9fed45
TLphongNguyen <acelinhphong@gmail.com>
2023-10-23 21:00:26 +0700

    test-2

---
+++ Apinguoidung/Solution1/DataModel/CustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class CustomerModel
+    {
+        public int MaChitietLogin { get; set; }
+        public int IDlogin { get; set; }
+        public string HoTen { get; set; }
+        public string DiaChi { get; set; }
+        public string SoDienThoai { get; set; }
+        public string Avatar { get; set; }
+    }
+}
